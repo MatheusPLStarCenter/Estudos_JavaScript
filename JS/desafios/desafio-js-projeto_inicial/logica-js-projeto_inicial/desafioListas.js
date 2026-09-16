10fbf8e02e1e191338bd93892d5590bdef8cc20a
Matheus Paulino de Lucena <matheus.lucena@starcenter.com.br>
2026-09-16 12:12:32 -0300

    Fix: Corrigindo arquivos dos desafios

--- JS/desafios/desafio-js-projeto_inicial/logica-js-projeto_inicial/desafioListas.js
+++
@@ -1,9 +0,0 @@
-let listaGenerica = [];
-let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
-linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
-console.log(linguagensDeProgramacao);
-
-let nomes = ["Roberta", "Leandro", "Kaique"];
-console.log(nomes[0]);
-console.log(nomes[1]);
-console.log(nomes[2]);
