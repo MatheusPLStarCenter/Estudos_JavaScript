8e3f93beb747c8f30f2784a90bf6b6f53cf70d3c
Matheus Paulino de Lucena <matheus.lucena@starcenter.com.br>
2026-09-21 13:25:31 -0300

    Tipo de variáveis no JavaScript

---
+++ JS/JS no BackEnd/3513-js-funds1/tipo-null-undefined.js
@@ -0,0 +1,13 @@
+let nomeEstudante;
+const professora = "Ana";
+
+console.log(nomeEstudante);
+console.log(typeof nomeEstudante);
+console.log(typeof professora);
+
+let telefoneEstudante = null;
+
+console.log(typeof telefoneEstudante);
+console.log(String(null));
+let frase = "POR FAVOR, NÃO GRITE!"
+console.log(frase.toLowerCase());
---
+++ JS/JS no BackEnd/3513-js-funds1/tipo-number.js
@@ -0,0 +1,10 @@
+const notaPrimerioBi = 8;
+const notaSegundoBi = 6.3;
+const notaTerceiroBi = -2;
+const notaQuartoBi = Number.parseInt("5");
+
+const total = (notaPrimerioBi + notaSegundoBi + notaTerceiroBi + notaQuartoBi) / 4;
+
+console.log("A média é " + total.toFixed(2));
+const nome = "Matheus";
+console.log("Meu nome é " + nome)
